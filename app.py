cd4afd6d54f7aeb50a07df23f83b43e208306e65
soumyajit1729 <soumyajit1729@gmail.com>
2023-06-19 20:07:37 +0530

    login system and multiple page

--- app.py
+++ app.py
@@ -36,18 +36,49 @@ def home():
     return render_template('home.html', itext = itext, 
                            keywords=keywords, text=para[:100], copy = copy_text)
 
+def get_text(login_url):
+  # Step 1: Send a GET request to the login page
+  response = requests.get(login_url)
+  # Define session
+  session = requests.session()
 
-def get_text_from_url(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+  # Step 2: Extract nonces from the response HTML
+  soup = BeautifulSoup(response.text, 'html.parser')
+  pf_nonce = soup.find('input', {'name': 'pf_nonce'})['value']
+  um_post_method_nonce = soup.find('input', {'name': 'um_post_method_nonce'})['value']
+  login_data = {
+      "user_login": "soumyajit1729",
+      "user_pass": "Sahaj@123",
+      "form_key": "Login",
+      "action_type": "login",
+      "user_id": 0,
+      "pf_nonce": pf_nonce,
+      "_wp_http_referer": "",
+      "method_name": "Login",
+      "um_post_method_nonce": um_post_method_nonce,
+      "um_submit_button": "Login"
+  }
+  wp_http_referer = login_url.replace("www.epicpeople.org","")
+  wp_http_referer = wp_http_referer.replace("epicpeople.org","")
+  wp_http_referer = wp_http_referer.replace("https://","")
 
-    # Find the main article content
-    article = soup.find('div', class_='entry-content')
+  login_data["_wp_http_referer"] = wp_http_referer
+  # Login to website
+  response = session.post(login_url, data=login_data)
+  soup = BeautifulSoup(response.text, "html.parser")
 
+  # Find article content
+  article_content = soup.find("div", {"class": "entry-content"})
+  print("//////////////////////////////////////////////////////////////")
+  print(login_url)
+  print(article_content.text[-100:])
+  return article_content
+
+def process(arg_article):
     # preprocessing
     a_tags = []
     flag = 0
-    for tag in article.descendants:
+    for tag in arg_article.descendants:
         # Do something with the tag
         if(tag.name=='h1' or tag.name=='h2' or tag.name=='h3' or tag.name=='strong'):
             txt = tag.text.lower()
@@ -61,11 +92,29 @@ def get_text_from_url(url):
         if not isinstance(tag, NavigableString):
             tag.decompose()
 
-    # Get the text of the article
-    article_text = article.get_text()
-    article_text = article_text.strip()
-    article_text = article_text.replace("\n", " ")
-    return article_text
+    # Get the text of the arg_article
+    arg_article_text = arg_article.get_text()
+    arg_article_text = arg_article_text.strip()
+    arg_article_text = arg_article_text.replace("\n", " ")
+    return arg_article_text
+
+
+def get_text_from_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    ppns = soup.find_all(class_='post-page-numbers')
+
+    # Find the main article content
+    article = get_text(url)
+    final_text = process(article)
+    if len(ppns)>1:
+        for i in range(2, len(ppns)+1):
+            new_link = url+str(i)+"/"
+            article_content = get_text(new_link)
+            final_text = final_text + " " + process(article_content)
+           
+    return final_text
+    
 
 @app.route('/get_link', methods=['GET','POST'])
 def get_link():
